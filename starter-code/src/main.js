dca18d4247cf9ce23844f40dbef991efcac82ab6
Francho Garcia <franchog79@gmail.com>
2019-06-23 18:36:28 +0200

    Finished practice myself but the shuffle algoritm must be improved

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,62 +1,96 @@
 var cards = [
-    { name: 'aquaman', img: 'aquaman.jpg' },
-    { name: 'batman', img: 'batman.jpg' },
-    { name: 'captain america', img: 'captain-america.jpg' },
-    { name: 'fantastic four', img: 'fantastic-four.jpg' },
-    { name: 'flash', img: 'flash.jpg' },
-    { name: 'green arrow', img: 'green-arrow.jpg' },
-    { name: 'green lantern', img: 'green-lantern.jpg' },
-    { name: 'ironman', img: 'ironman.jpg' },
-    { name: 'spiderman', img: 'spiderman.jpg' },
-    { name: 'superman', img: 'superman.jpg' },
-    { name: 'the avengers', img: 'the-avengers.jpg' },
-    { name: 'thor', img: 'thor.jpg' },
-    { name: 'aquaman', img: 'aquaman.jpg' },
-    { name: 'batman', img: 'batman.jpg' },
-    { name: 'captain america', img: 'captain-america.jpg' },
-    { name: 'fantastic four', img: 'fantastic-four.jpg' },
-    { name: 'flash', img: 'flash.jpg' },
-    { name: 'green arrow', img: 'green-arrow.jpg' },
-    { name: 'green lantern', img: 'green-lantern.jpg' },
-    { name: 'ironman', img: 'ironman.jpg' },
-    { name: 'spiderman', img: 'spiderman.jpg' },
-    { name: 'superman', img: 'superman.jpg' },
-    { name: 'the avengers', img: 'the-avengers.jpg' },
-    { name: 'thor', img: 'thor.jpg' }
+    { name: "aquaman", img: "aquaman.jpg" },
+    { name: "batman", img: "batman.jpg" },
+    { name: "captain america", img: "captain-america.jpg" },
+    { name: "fantastic four", img: "fantastic-four.jpg" },
+    { name: "flash", img: "flash.jpg" },
+    { name: "green arrow", img: "green-arrow.jpg" },
+    { name: "green lantern", img: "green-lantern.jpg" },
+    { name: "ironman", img: "ironman.jpg" },
+    { name: "spiderman", img: "spiderman.jpg" },
+    { name: "superman", img: "superman.jpg" },
+    { name: "the avengers", img: "the-avengers.jpg" },
+    { name: "thor", img: "thor.jpg" },
+    { name: "aquaman", img: "aquaman.jpg" },
+    { name: "batman", img: "batman.jpg" },
+    { name: "captain america", img: "captain-america.jpg" },
+    { name: "fantastic four", img: "fantastic-four.jpg" },
+    { name: "flash", img: "flash.jpg" },
+    { name: "green arrow", img: "green-arrow.jpg" },
+    { name: "green lantern", img: "green-lantern.jpg" },
+    { name: "ironman", img: "ironman.jpg" },
+    { name: "spiderman", img: "spiderman.jpg" },
+    { name: "superman", img: "superman.jpg" },
+    { name: "the avengers", img: "the-avengers.jpg" },
+    { name: "thor", img: "thor.jpg" }
 ];
 var memoryGame = new MemoryGame(cards);
 
 const flipCard = card => {
-    let cardSelected = card.children[0]
-    let frontCart = cardSelected.classList[0]
-    console.log(cardSelected)
-    console.log(card.children[1])
-    let backCart = card.children[1].classList[0]
+    let cardSelected = card.children[0];
+    let frontCart = cardSelected.classList[0];
+    let backCart = card.children[1].classList[0];
 
     //flips the cards
-    cardSelected.classList = backCart
-    card.children[1].classList = frontCart
-}
+    cardSelected.classList = backCart;
+    card.children[1].classList = frontCart;
+};
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    var html = '';
+    var html = "";
     memoryGame.cards.forEach(function(pic) {
         html += '<div class="card" data-card-name="' + pic.name + '">';
         html += '  <div class="back" name="' + pic.img + '"></div>';
-        html += '  <div class="front" style="background: url(img/' + pic.img + ') no-repeat"></div>';
-        html += '</div>';
+        html +=
+            '  <div class="front" style="background: url(img/' +
+            pic.img +
+            ') no-repeat"></div>';
+        html += "</div>";
     });
 
     // Add all the div's to the HTML
-    document.querySelector('#memory_board').innerHTML = html;
+    document.querySelector("#memory_board").innerHTML = html;
 
     // Bind the click event of each element to a function
-    document.querySelectorAll('.card').forEach(function(card) {
-        card.onclick = function() {
+    document.querySelectorAll(".card").forEach(function(card) {
+        card.onclick = function(e) {
             // TODO: write some code here
-
-            flipCard(card)
-            console.log('Card clicked')
-        }
+            if (memoryGame.pairsClicked < 2) {
+                flipCard(card);
+                memoryGame.pickedCards.push(card);
+                memoryGame.pairsClicked += 1;
+                if (memoryGame.pairsClicked == 2) {
+                    let valueCardOne = memoryGame.pickedCards[0].getAttribute(
+                        "data-card-name"
+                    );
+                    let valueCardTwo = memoryGame.pickedCards[1].getAttribute(
+                        "data-card-name"
+                    );
+                    if (memoryGame.checkIfPair(valueCardOne, valueCardTwo)) {
+                        if (memoryGame.isFinished()) {
+                            console.log("YOU WIN!");
+                        }
+                        memoryGame.pairsClicked = 0;
+                    } else {
+                        memoryGame.pickedCards.forEach(pickedCard => {
+                            var matches = document.querySelectorAll(
+                                `div[data-card-name='${pickedCard.getAttribute(
+                  "data-card-name"
+                )}'`
+                            );
+                            matches.forEach(match => {
+                                setTimeout(() => {
+                                    if (match.children[0].classList[0] == "front") {
+                                        flipCard(match);
+                                        memoryGame.pairsClicked = 0;
+                                    }
+                                }, 500);
+                            });
+                        });
+                    }
+                    memoryGame.pickedCards = [];
+                }
+            }
+        };
     });
-})
+});
